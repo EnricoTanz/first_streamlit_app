aae074f7fa898896f64a2b79e0ae6940a5529e8b
EnricoTanz <122734314+EnricoTanz@users.noreply.github.com>
2023-12-12 16:44:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("miao miao")
-streamlit.text("bau bau")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
