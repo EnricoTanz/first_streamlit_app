6b559351d0d63768f1f71a13bf61a8d7168260ca
EnricoTanz <122734314+EnricoTanz@users.noreply.github.com>
2023-12-13 17:26:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,9 @@ streamlit.dataframe(fruits_to_show)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+fruit_choice2 = streamlit.text_input('What fruit would you like to destroy?','Jackfruit')
+streamlit.write('The user entered ', fruit_choice2)
+
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
